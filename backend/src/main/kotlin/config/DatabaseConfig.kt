94eeff8f60b1a6cec556bf843125bbd8b3873bd6
ng7g22 <golshahi@icloud.com>
2025-03-18 14:06:13 +0000

    Added fetching from appointments container and audiologists container

--- backend/src/main/kotlin/config/DatabaseConfig.kt
+++ backend/src/main/kotlin/config/DatabaseConfig.kt
@@ -22,6 +22,12 @@ object DatabaseConfig {
         throw IllegalArgumentException("Users Container name is missing")
     val hearingAidsContainer: String = dotenv["HEARING_AID_CONTAINER"] ?: 
         throw IllegalArgumentException("Hearing Aid Container name is missing")
+    val appointmentsContainer: String = dotenv["APPOINTMENTS_CONTAINER"] ?: 
+        throw IllegalArgumentException("Appointments container name is missing")
+    val appointmentTypesContainer: String = dotenv["APPOINTMENT_TYPES_CONTAINER"] ?: 
+        throw IllegalArgumentException("Appointment types container name is missing")
+    val audiologistsContainer: String = dotenv["AUDIOLOGISTS_CONTAINER"] ?: 
+        throw IllegalArgumentException("Audiologists container name is missing")
 
     // Create a Cosmos client using the provided credentials
     val cosmosClient: CosmosClient = CosmosClientBuilder()
@@ -37,4 +43,10 @@ object DatabaseConfig {
     fun getUsersContainer(): CosmosContainer = database.getContainer(usersContainer)
     
     fun getHearingAidsContainer(): CosmosContainer = database.getContainer(hearingAidsContainer)
+
+    fun getAppointmentsContainer(): CosmosContainer = database.getContainer(appointmentsContainer)
+
+    fun getAppointmentTypesContainer(): CosmosContainer = database.getContainer(appointmentTypesContainer)
+
+    fun getAudiologistsContainer(): CosmosContainer = database.getContainer(audiologistsContainer)
 }
