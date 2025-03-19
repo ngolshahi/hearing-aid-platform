07930c99d280379126e7080d47b4ea3a40c7cf81
ng7g22 <golshahi@icloud.com>
2025-03-19 11:14:17 +0000

    User | Audiologist is stored in local storage instead of AuthResponse

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,19 +1,48 @@
 // src/contexts/AuthContext.tsx
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { getCurrentUser, AuthResponse, logout } from '../services/authService';
+import { UserDetails } from '../services/appointmentService';
 
 interface AuthContextType {
-  currentUser: AuthResponse | null;
+  currentUser: User | Audiologist | null;
   isAuthenticated: boolean;
   loading: boolean;
   logout: () => void;
-  setCurrentUser: (user: AuthResponse | null) => void;
+  setCurrentUser: (user: User | Audiologist | null) => void;
+}
+
+interface WorkHours {
+  start: string;
+  end: string;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  phone?: string;
+  image?: string;
+  details?: UserDetails;
+}
+
+
+interface Audiologist {
+  id: string;
+  name: string;
+  image: string;
+  description: string;
+  qualifications: string;
+  email: string;
+  phone: string;
+  workSchedule: Record<string, WorkHours>;
+  details?: UserDetails;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState<AuthResponse | null>(null);
+  const [currentUser, setCurrentUser] = useState<User | Audiologist | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -27,7 +56,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
 
   const value = {
     currentUser,
-    isAuthenticated: !!currentUser?.token,
+    isAuthenticated: !!currentUser?.email,
     loading,
     logout: () => {
       logout();
