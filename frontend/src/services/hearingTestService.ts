7b56dd0bf4d2be769c6477c83dd72a168537b559
ng7g22 <golshahi@icloud.com>
2025-05-14 12:14:40 +0100

    Removed unecessary import

--- frontend/src/services/hearingTestService.ts
+++ frontend/src/services/hearingTestService.ts
@@ -1,4 +1,3 @@
-import { API_BASE_URL } from './config';
 import { SpeechConfig, SpeechSynthesizer, AudioConfig, ResultReason, SpeechRecognizer } from 'microsoft-cognitiveservices-speech-sdk';
 
 export interface ToneTestResult {
