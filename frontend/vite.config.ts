d2a814c765c2813823d8c2cbc4089e04bd0bd195
ng7g22 <golshahi@icloud.com>
2025-05-14 12:14:31 +0100

    Changed api url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'http://192.168.0.244:8080',
+        target: 'http://192.168.0.13:8080',
         changeOrigin: true,
         secure: false
       }
